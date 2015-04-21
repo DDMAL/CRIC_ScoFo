7878de6886d2e3bfe61a3c5632394abdabbea9fd
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2015-04-21 13:57:35 -0400

    Fixes: delay time for midi actions are now properly parsed. Close #13.
    Signed-off-by: Gabriel Vigliensoni <gabriel@vigliensoni.com>

--- scripts/midiactions_parser.py
+++ scripts/midiactions_parser.py
@@ -14,9 +14,9 @@ def durationparser(inputfile):
     ; delay msgrecvr pitch velocity duration
 
     """
-    # 1. All data in a single dataframe
     inputfile = open(inputfile)
     lines = inputfile.readlines()
+    inputfile.close()
     lines = lines[2:-1]  # only lines without headers and final bracket
     parsedlines = []
     for line in lines:
@@ -26,22 +26,38 @@ def durationparser(inputfile):
         el[3] = int(el[3])
         parsedlines.append(el)
     df = pd.DataFrame(data=parsedlines, columns=['del', 'mes', 'pit', 'vel'])
-    dfd = pd.DataFrame(columns=['del', 'mes', 'pit', 'vel', 'dur'])  # new dataframe with duration
-    note_on_df = df[df['vel'] > 0]   # dataframe of NOTEONs only
-    for i in xrange(len(note_on_df)):
-        non = note_on_df.irow(i)
-        non_idx = non.name  # index of this NOTE ON in the dataframe
-        pit = non.pit  # note pitch value
-        # and search for the next index with pitch value 0 in the original df
-        aln_df = df.loc[non_idx:]  # all notes dataframe staring at the index of the NOTE ON
-        # search for the next NOTE index with [pitch == pit, vel = 00]
-        nof = aln_df.loc[non_idx:][(aln_df.loc[non_idx:]['vel'] == 0) & (aln_df.loc[non_idx:]['pit'] == pit)].irow(0)
+    # 1 Final dataset copy of NoteONs
+    note_on_df = df[df['vel'] > 0]
+    out_df = pd.DataFrame(data=note_on_df, columns=['del', 'mes', 'pit', 'vel', 'dur'])
+    final_df = pd.DataFrame(columns=['del', 'mes', 'pit', 'vel', 'dur'])
+    ddl = 0
+    for i in xrange(len(out_df)):
+        # Finding the indexes of NOTEON and NOTEOFF
+        non = out_df.irow(i)
+        non_pit = non.pit
+        non_idx = non.name
+
+        nof = df.loc[non_idx:][(df.loc[non_idx:]['vel'] == 0) & (df.loc[non_idx:]['pit'] == non_pit)].irow(0)
         nof_idx = nof.name
-        #  the final duration for a NOTE ON is given by adding all delays between ]NOTEON idx, NOTEOFF idx]
+        # duration of note is the sum of all delays between ]NOTEON, NOTEFF]
         dur = sum(df[non_idx + 1:nof_idx + 1]['del'])
-        dfd.loc[i] = note_on_df.irow(i)  #  store the NOTEON and duration in a new dataframe
-        dfd.loc[i, 'dur'] = dur
-    return dfd
+        # new delay time depends if its a note or a chord
+        if df.loc[non_idx, 'del'] == 0:
+            # if it's a chord, ddl = 0
+            ddl = 0
+        else:
+            # if it's a note, ddl = current ddl + previous ddl>0
+            for j in xrange(non_idx, 0, -1):
+                if df.loc[j - 1, 'del'] != 0:
+                    prev_ddl = df.loc[j - 1, 'del']
+                    ddl = df.loc[non_idx, 'del'] + prev_ddl
+                    break
+        final_df.loc[i] = ddl, 'mnote', non_pit, 80, dur
+        # print("ddl: {4}, dur: {2}, non_idx: {0}, nof_idx: {1}, pit: {3}").format(non_idx, nof_idx, dur, non_pit, ddl)
+
+    final_df.loc[0, 'del'] = note_on_df.loc[0, 'del']
+    return final_df
+
 
 if __name__ == "__main__":
     """
